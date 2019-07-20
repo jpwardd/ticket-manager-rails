ce6d1794203d8017f3a8ae5e823320c6f0e117d7
jpwardd <jward9421@gmail.com>
2019-07-19 20:41:48 -0400

    Login component converted to a functional component with hooks

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,11 +4,12 @@ import Dashboard from './pages/dashboard/Dashboard';
 import Login from './pages/auth/Login';
 
 
+
 function App() {
   return (
     <div className="App">
       <Router>
-        <Route path="/" component={Login} />
+        <Route exact path="/" component={Login} />
         <Switch>
           <Route exact path="/dashboard" component={Dashboard} />
         </Switch>
